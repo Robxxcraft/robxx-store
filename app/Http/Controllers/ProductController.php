79e8af0c93b33abfe3761a8477ccb59dc3614787
Robxx3101 <robbiechandra02@gmail.com>
2021-11-07 18:15:04 +0700

    testss

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -78,8 +78,8 @@ class ProductController extends Controller
                     'width' => 600,
                     'heigth' => 400,
                 ]
-            ])->getSecurePath();
-            $product->photo = $uploadImg;
+            ]);
+            $product->photo = $uploadImg->getSecurePath();
             $product->publicId = $uploadImg->getPublicId();
             $product->save();
         }
