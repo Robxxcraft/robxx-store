e19664c792b455ed9c72b65225c76b3a8ad529d1
Robxx3101 <robbiechandra02@gmail.com>
2021-11-22 17:08:53 +0700

    update offline

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,6 +9,8 @@ class Tag extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'slug'];
+
     public function product()
     {
         return $this->belongsToMany(Product::class)->with('category');
